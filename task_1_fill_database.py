b1d75bfbe19ac4702048a33f3be1cd0d6ad92576
Ana Stakic <anastakic26@gmail.com>
2021-06-06 23:51:21 +0200

    Insert into database

--- task_1_fill_database.py
+++ task_1_fill_database.py
@@ -8,10 +8,50 @@ PASSWORD = 'VeryStrongPassword1.'
 
 class DataBase:
 
-    @staticmethod
-    def insert_data(data):
+    def __init__(self):
+        self.queries = []
+
+    def parse_value(self, value, i):
+        # for i between 0 and 5 strings
+        # for i equals 6 boolean
+        # for i larger than 6 int / float
+        if value is None:
+            return 'NULL'
+
+        if i <= 5:
+            return '"' + value + '"'
+        elif i == 6:
+            return value
+        else:
+            return value
+
+    def add_data(self, data):
+        insert_into_query = \
+            'INSERT INTO `psz_db`.`realty`' \
+            '(`type`,' \
+            '`offer_type`,' \
+            '`heating_type`,' \
+            '`city`,' \
+            '`quarter`,' \
+            '`webpage`,' \
+            '`registered`,' \
+            '`square_meters`,' \
+            '`year_built`,' \
+            '`land_area`,' \
+            '`total_floors`,' \
+            '`floor`,' \
+            '`total_rooms`,' \
+            '`total_bathrooms`,' \
+            '`price`)' \
+            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)' \
+            % (tuple([self.parse_value(single_data, i) for i, single_data in enumerate(data)]))
+        print(insert_into_query)
+        self.queries.append(insert_into_query)
+
+    def insert_data(self):
         connection = cursor = None
         try:
+            print("insert!")
             connection = pyodbc.connect('DRIVER={MySQL ODBC 8.0 ANSI Driver};'
                                         'SERVER=' + SERVER + ';' +
                                         'DATABASE=' + DATABASE + ';' +
@@ -21,6 +61,11 @@ class DataBase:
 
             # insert into table
 
+            for query in self.queries:
+                print(cursor.execute(query))
+
+            connection.commit()
+
         except (Exception, pyodbc.DatabaseError) as error:
             print("Error importing data", error)
         finally:
@@ -29,8 +74,8 @@ class DataBase:
                 cursor.close()
                 connection.close()
 
-    @staticmethod
-    def empty_database():
+
+    def empty_database(self):
         connection = cursor = None
         try:
             connection = pyodbc.connect('DRIVER={MySQL ODBC 8.0 ANSI Driver};'
@@ -42,12 +87,18 @@ class DataBase:
 
             # delete from table
 
+            clean_database = 'delete from "realty"'
+            cursor.execute(clean_database)
+            connection.commit()
+            print("Database_empty: ")
+
         except (Exception, pyodbc.DatabaseError) as error:
             print("Error emptying database", error)
+
         finally:
+
             # closing database connection.
+
             if connection:
                 cursor.close()
                 connection.close()
-
-
