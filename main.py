50b16366f7377a47fdbdcc7bdeaecb5bdbb53272
Ana Stakic <anastakic26@gmail.com>
2021-06-10 12:24:38 +0200

    main fuction

---
+++ main.py
@@ -0,0 +1,13 @@
+from task_1_data_collecting import DataCollector
+from task_2_data_analysis import DataAnalysis
+
+
+def main():
+    # data_collector = DataCollector()
+    # data_collector.start()
+    data_analysis = DataAnalysis()
+    data_analysis.do_analysis()
+
+
+if __name__ == '__main__':
+    main()
